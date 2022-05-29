6a66e0c7d526241b295d3baccb4a1677819d84ef
HU <nuosifalatu@outlook.com>
2022-05-29 17:30:03 +0800

    source提交

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(121, 574);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(912, 574);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 67);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "End";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblBlue);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("仿宋", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox1.Location = new System.Drawing.Point(121, 176);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(937, 338);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "双色球号码";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(51, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(152, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(256, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 43);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(354, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 43);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(459, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 43);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(566, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 43);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "00";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.ForeColor = System.Drawing.Color.Blue;
+            this.lblBlue.Location = new System.Drawing.Point(849, 114);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(63, 43);
+            this.lblBlue.TabIndex = 6;
+            this.lblBlue.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 807);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBlue;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string[] redNums = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"
+        ,"11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
+        ,"21", "22", "23", "24", "25", "26", "27", "28", "29", "30"
+        ,"31", "32", "33"};
+
+        string[] blueNums = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"
+        ,"11", "12", "13", "14", "15", "16"};
+
+        private static readonly Object OSS_LOCK = new Object();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TaskFactory taskFactory = new TaskFactory();
+
+            isStop = false;
+
+            foreach (var item in groupBox1.Controls)
+            {
+                if (item is Label)
+                {
+                    if ("lblBlue".Equals(((Label)item).Name))
+                    {
+                        taskFactory.StartNew(() =>
+                        {
+                            while (true)
+                            {
+                                int index = GetIndexNumber(0, blueNums.Length);
+
+                                string text = blueNums[index];
+
+                                if (!isStop)
+                                {
+                                    UpdateLabelText((Label)item, text);
+                                }
+                            }
+                        });
+                    }
+                    else
+                    {
+                        taskFactory.StartNew(() =>
+                        {
+                            while (true)
+                            {
+                                int index = GetIndexNumber(0, redNums.Length);
+
+                                string text = redNums[index];
+                                if (!isStop)
+                                {
+                                    lock (OSS_LOCK)
+                                    {
+                                        if (isExsited(text))
+                                        {
+                                            continue;
+                                        }
+
+                                        UpdateLabelText((Label)item, text);
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        }
+
+        private bool isStop = false;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isStop = true;
+        }
+
+        public bool isExsited(string text)
+        {
+            foreach (var item in groupBox1.Controls)
+            {
+                if (item is Label && ((Label)item).Text.Equals(text))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void UpdateLabelText(Label label,string text)
+        {
+            if (label.InvokeRequired)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    if (!isStop)
+                    {
+                        label.Text = text;
+                    }
+                }));
+            }
+            else
+            {
+                label.Text = text;
+            }
+        }
+        public int GetIndexNumber(int min,int max)
+        {
+            Thread.Sleep(1000);
+
+            Guid guid = Guid.NewGuid();
+            int seek = DateTime.Now.Millisecond;
+            foreach (var item in guid.ToString())
+            {
+                switch (item)
+                {
+                    case 'a':
+                    case 'b':
+                    case 'c':
+                    case 'd':
+                    case 'e':
+                    case 'f':
+                    case 'g':
+                        seek += 1;
+                        break;
+                    case 'h':
+                    case 'i':
+                    case 'j':
+                    case 'k':
+                    case 'l':
+                    case 'm':
+                    case 'n':
+                        seek += 2;
+                        break;
+                    case 'o':
+                    case 'p':
+                    case 'q':
+                    case 'r':
+                    case 's':
+                    case 't':
+                    case 'u':
+                        seek += 3;
+                        break;
+                    case 'v':
+                    case 'w':
+                    case 'x':
+                    case 'y':
+                    case 'z':
+                        seek += 4;
+                        break;
+                    default:
+                        seek += 5;
+                        break;
+                }
+            }
+
+            Random random = new Random(seek);
+            return random.Next(min, max);
+        }
+
+    }
+}
